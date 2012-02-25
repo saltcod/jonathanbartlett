b04e30231b9d80661d9e4f3f4dcbefb15692d0fc
Terry Sutton <saltcod@gmail.com>
2012-02-24 22:07:10 -0330

    Fixed up a bad merge

--- js/jonathanbartlett.js
+++ js/jonathanbartlett.js
@@ -28,11 +28,12 @@ jQuery(function($){
 	//Set the height of the sidebar == to the window height
 	$('#secondary').css({ 'height':(( $(window).height())) });
 
+ 
  //Portfolio menu scrollability
 	$(function(){
 		$('.menu-portfolio-container').jScrollPane();
 	}); 	 
-
+ 
 	//Do some hide and seek on "MORE PROJECTS" in the sidebar
 	
 	$('#secondary').hover(
